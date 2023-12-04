2761e4e782746ab7e393f558788422a12a3e5070
Vladyslav Vlasenko <fl4shqq@gmail.com>
2023-12-04 13:37:36 +0200

    update variables in config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = os.environ.get('SQLALCHEMY_ECHO')
